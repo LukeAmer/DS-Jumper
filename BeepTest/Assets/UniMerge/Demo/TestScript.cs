627a1182a0feaa79dfe6cee8b4bc080d945f441c
Luke Amer <lukeamer@me.com>
2015-05-20 20:35:11 +0100

    .

--- BeepTest/Assets/UniMerge/Demo/TestScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TestScript : MonoBehaviour {
-	public GameObject ref1, ref2, ref3;
-	public Collider col1;
-	public Rigidbody rigid;
-	public Bounds bounds;
-	public Quaternion quat;
-}
