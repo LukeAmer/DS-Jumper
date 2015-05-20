a83e180c350a99ba7b0da3dc8d5a2c6889dab800
Luke Amer <lukeamer@me.com>
2015-05-21 00:20:50 +0100

    Updates

---
+++ Infiltrator/Assets/Scripts/Environment.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Environment : MonoBehaviour 
+{
+	// Public Variables
+	public GameObject leftLine;
+	public GameObject rightLine;
+
+	// Private Variables
+	float height = 0.0f;
+	float width = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		height = 2f * Camera.main.orthographicSize;
+		width = height * Camera.main.aspect;
+
+		rightLine.transform.position = new Vector2 (width/2.0f, 0.0f);
+		leftLine.transform.position = new Vector2 (-width/2.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Infiltrator/Assets/Scripts/GUIControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIControl : MonoBehaviour 
+{
+	// Public Variables
+	public Text fpsText;
+
+	// Private Variables
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		fpsText.text =  (1.0/Time.deltaTime).ToString();
+	}
+}
